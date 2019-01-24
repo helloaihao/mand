b9a588c1cac77ec1c6ab6a2c13247b38250fd449
maxai <maxai@futunn.com>
2019-01-24 21:37:18 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible';
 
 Vue.config.productionTip = false
+
+import {
+  Agree,
+  ActionBar,
+  Field,
+  FieldItem,
+  InputItem,
+  Picker
+} from 'mand-mobile';
+
+Vue.component(Agree.name, Agree);
+Vue.component(ActionBar.name, ActionBar);
+Vue.component(Field.name, Field);
+Vue.component(FieldItem.name, FieldItem);
+Vue.component(InputItem.name, InputItem);
+Vue.component(Picker.name, Picker);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <md-field title="投保人" class="block">
+      <md-input-item
+        title="投保人姓名"
+        placeholder="请填写投保人姓名"
+      ></md-input-item>
+      <md-input-item
+        title="身份证号"
+        placeholder="请填写投保人身份证号"
+      ></md-input-item>
+    </md-field>
+    <md-field title="被保人" class="block">
+      <md-input-item
+        title="被保人姓名"
+        placeholder="请填写被保人姓名"
+      ></md-input-item>
+      <md-field-item
+        title="与投保人关系"
+        arrow="arrow-right"
+        :value="relation"
+        @click="isPickerShow = true"
+        solid
+      ></md-field-item>
+      <md-picker
+        v-model="isPickerShow"
+        :data="pickerData"
+        title="选择与投保人关系"
+      ></md-picker>
+      <md-input-item
+        title="身份证号"
+        placeholder="请填写被保人身份证号"
+      ></md-input-item>
+      <md-input-item
+        title="手机号"
+        type="phone"
+        placeholder="请填写被保人手机号"
+      ></md-input-item>
+    </md-field>
+    <md-action-bar :actions="actionBarData">
+      &yen;128.00
+    </md-action-bar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+
+  data () {
+    return {
+      relation: '本人',
+      isPickerShow: false,
+      actionBarData: [{
+        text: '我要投保'
+      }],
+      pickerData: [[{text:'本人'},{text:'父母'},{text:'配偶'},{text:'子女'}]],
+      agreeConf: {
+        checked: true,
+        name: 'agree0',
+        disabled: false,
+        introduction: '选中状态',
+      }
+    }
+  },
+  methods: {
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+body{
+  background: #f0f0f0;
+}
+.detail{
+  background: #fff;
+}
+.block{
+  font-size: .35rem;
+  margin-top: .32rem;
+}
+.agree{
+  padding: .32rem;
+  font-size: .32rem;
+  font-size: .24rem;
+  color: #666;
+}
+</style>
